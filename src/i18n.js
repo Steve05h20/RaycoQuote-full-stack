3de283e8416bb4d8736ca82e8acd3eda574d61ca
Steve05h20 <steve05h2o@gmail.com>
2024-09-17 15:55:02 -0400

    update

--- src/i18n.js
+++ src/i18n.js
@@ -15,7 +15,7 @@ i18n
             escapeValue: false,
         },
         backend: {
-            loadPath: 'src/locales/{{lng}}/{{ns}}.json',
+            loadPath: 'public/locales/{{lng}}/{{ns}}.json',
         },
     });
 
